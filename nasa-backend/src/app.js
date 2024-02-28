036a2f301d9b02bd6286d7cfef203d0bec682ec6
Gabriel <prismaticnetwork12@gmail.com>
2024-02-28 18:24:10 -0300

    feat: add CORS

--- nasa-backend/src/app.js
+++ nasa-backend/src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const { planetRouter } = require('./controllers/planets.controller');
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 app.use(planetRouter);
 module.exports = app;
